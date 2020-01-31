5501171dede4f09781070ffbfd14aaa66245cba9
BurnerB <barnabykamau@gmail.com>
2020-01-31 11:50:46 +0300

    create user controller -create form with Vform -create user route

--- resources/js/components/users.vue
+++ resources/js/components/users.vue
@@ -58,13 +58,68 @@
                     <span aria-hidden="true">&times;</span>
                     </button>
                 </div>
-                <div class="modal-body">
-                    ...
-                </div>
-                <div class="modal-footer">
-                    <button type="button" class="btn btn-danger" data-dismiss="modal">Close</button>
-                    <button type="button" class="btn btn-primary">Create</button>
-                </div>
+                <form @submit.prevent="createUser">
+                  <div class="modal-body">
+                      <div class="form-group">
+                        <!-- 2 way modal binding -->
+                        <input v-model="form.name" type="text" name="name"
+                        placeholder="Name"
+                          class="form-control" :class="{ 'is-invalid': form.errors.has('name') }">
+                        <has-error :form="form" field="name"></has-error>
+                      </div>
+
+                      <div class="form-group">
+                        <!-- 2 way modal binding -->
+                        <input v-model="form.email" type="text" name="email"
+                        placeholder="Email"
+                          class="form-control" :class="{ 'is-invalid': form.errors.has('email') }">
+                        <has-error :form="form" field="name"></has-error>
+                      </div>
+                  
+
+                    <div class="form-group">
+                          <!-- 2 way modal binding -->
+                          <input v-model="form.password" type="password" name="password"
+                          placeholder="Password"
+                            class="form-control" :class="{ 'is-invalid': form.errors.has('password') }">
+                          <has-error :form="form" field="password"></has-error>
+                    </div>
+
+                    <div class="form-group">
+                          <!-- 2 way modal binding -->
+                          <input v-model="form.type" type="text" name="type"
+                          placeholder="Type"
+                            class="form-control" :class="{ 'is-invalid': form.errors.has('type') }">
+                          <has-error :form="form" field="type"></has-error>
+                    </div>
+
+                    <div class="form-group">
+                          <select name="type" v-model="form.type" class="form-control" :class="{
+                            'is-invalid':form.errors.has('type')}">
+                            <option value="">Select User role</option>
+                            <option value="admin">Admin</option>
+                            <option value="user">User</option>
+                            <option value="author">Author</option>
+                          </select>
+                          <has-error :form="form" field="bio"></has-error>
+                    </div>
+
+                    <div class="form-group">
+                          <!-- 2 way modal binding -->
+                          <textarea v-model="form.bio" type="text" name="bio"
+                          placeholder=" short bio(optional)"
+                            class="form-control" :class="{ 'is-invalid': form.errors.has('bio') }"></textarea>
+                          <has-error :form="form" field="bio"></has-error>
+                    </div>
+
+                    
+                  </div>
+                
+                  <div class="modal-footer">
+                      <button type="button" class="btn btn-danger" data-dismiss="modal">Close</button>
+                      <button type="button" class="btn btn-primary">Create</button>
+                  </div>
+                </form>
                 </div>
             </div>
             </div>
@@ -74,7 +129,25 @@
 </template>
 
 <script>
+    import Form from 'vform';
     export default {
+      data(){
+        return{
+          form: new Form({
+            name:'',
+            email : '',
+            password : '',
+            type:'',
+            bio :'',
+            photo : ''
+          })
+        }
+      },
+      methods:{
+        createUser(){
+                this.form.post('api/login')
+        }
+      },
         mounted() {
             console.log('Component mounted.')
         }
